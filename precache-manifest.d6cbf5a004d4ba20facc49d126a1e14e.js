264f1591f5bcd353f332d3582c09e1440110d3a1
rjfm <javier.fuentesm@hotmail.com>
2019-11-08 23:07:43 -0600

    Updates

---
+++ precache-manifest.d6cbf5a004d4ba20facc49d126a1e14e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f639ebaaac04395debb1450eecbbc434",
+    "url": "/Intelimetrica/index.html"
+  },
+  {
+    "revision": "7d4d792eaf053457d6a7",
+    "url": "/Intelimetrica/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "07b12f4243dbac65631a",
+    "url": "/Intelimetrica/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7d4d792eaf053457d6a7",
+    "url": "/Intelimetrica/static/js/2.49485f6e.chunk.js"
+  },
+  {
+    "revision": "07b12f4243dbac65631a",
+    "url": "/Intelimetrica/static/js/main.6b044ec2.chunk.js"
+  },
+  {
+    "revision": "967d4ca4ef9105acaccb",
+    "url": "/Intelimetrica/static/js/runtime-main.8a023177.js"
+  }
+]);
